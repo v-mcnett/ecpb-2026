2e2129212c3a20acbd7289355b43d0dbc0ab2de3
v-mcnett <vmcnett@gmail.com>
2026-08-08 15:53:28 -0400

    Opening facebook, instagram and pinterest in new tabs

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -14,16 +14,16 @@ export default function Footer() {
         <div className="grid grid-flow-col gap-4">
           
 
-          <a href="https://facebook.com/emeraldcityphotobooth" className="link link-hover ">
+          <a href="https://facebook.com/emeraldcityphotobooth" className="link link-hover " target="_blank" rel="noopener noreferrer">
             <span className="hidden md:block lg:block mb-1">Facebook</span>
             <span className="inline-flex items-center"><SiFacebook className="flex items-center w-6 h-6"  /></span>
           </a>
-          <a href="https://instagram.com/emeraldcityphotobooth" className="link link-hover">
+          <a href="https://instagram.com/emeraldcityphotobooth" className="link link-hover" target="_blank" rel="noopener noreferrer">
             <span className="hidden md:block lg:block mb-1">Instagram</span>
             <span className="inline-flex items-center"><SiInstagram className="flex items-center w-6 h-6"  /></span>
 
           </a>
-          <a href="https://pinterest.com/emeraldcitywed" className="link link-hover">
+          <a href="https://pinterest.com/emeraldcitywed" className="link link-hover" target="_blank" rel="noopener noreferrer">
             <span className="hidden md:block lg:block mb-1">Pinterest</span>
             <span className="inline-flex items-center"><SiPinterest className="flex items-center w-6 h-6"  /></span>
           </a>
